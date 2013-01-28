902d079ea587c471d2f3d8abe855e08dface333a
Vladislav Vasilenko <xyzslash@gmail.com>
2013-01-28 15:46:33 +0400

    Lesson 3 Fullscreen Application

--- OpenTK_Example1/Main.cs
+++ OpenTK_Example1/Main.cs
@@ -1,65 +1,52 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-
-
 using OpenTK;
 using OpenTK.Graphics;
 using OpenTK.Graphics.OpenGL;
 
-namespace OpenTK_Example1
+class Program
 {
-	public class Program
+	
+	static void Main(string[] args)
 	{
-		static uint myBufferHandle = 0;
-		
-		static void dataPreparation()
+		using(MyWindow N = new MyWindow())
 		{
-			GL.GenBuffers(1, out myBufferHandle);
-			
-			GL.BindBuffer(BufferTarget.ArrayBuffer, myBufferHandle);
-			
-			var myTriangleVerticies = new List<Vector2d>()
-			{
-				new Vector2d(-1, -1),
-				new Vector2d(1, -1),
-				new Vector2d(0, 1)
-			};
-			
-			GL.BufferData<Vector2d>(BufferTarget.ArrayBuffer,
-			                        new IntPtr(
-				myTriangleVerticies.Count * sizeof(double) * 2),
-			                        myTriangleVerticies.ToArray(),
-			                        BufferUsageHint.StaticDraw);
-			
-		}
-		
-		static void Main (string [] args)
-		{
-			var myTKWindow = new GameWindow(640, 480, GraphicsMode.Default, "My First Game");
-			
-			dataPreparation();
-			
-			myTKWindow.RenderFrame += (object sender, FrameEventArgs e) => 
-			{
-				GL.ClearColor(Color4.Blue);
-				GL.Clear (ClearBufferMask.ColorBufferBit);
-				
-				GL.EnableClientState(ArrayCap.VertexArray);
-				
-				GL.BindBuffer(BufferTarget.ArrayBuffer, myBufferHandle);
-				GL.VertexPointer(2, VertexPointerType.Double, 0, 0);
-				GL.DrawArrays(BeginMode.Triangles, 0, 3);
-				
-				GL.DisableClientState(ArrayCap.VertexArray);
-				
-				myTKWindow.SwapBuffers();
-			};
-			
-			myTKWindow.Run();
-			
-			GL.DeleteBuffers(1, ref myBufferHandle);
+			N.Run(100.0);
 		}
 	}
+	
 }
 
+// This class is a powerful window that can be created without using any GUI library.
+public class MyWindow : GameWindow
+{
+	public MyWindow()
+		: base()
+	{
+		Console.WriteLine("Press any key to exit.");
+		KeyPress += HandleKeyPressEvent;
+		Load += HandleOnLoadEvent;
+		RenderFrame += HandleRenderEvent;
+	}
+	
+	public void HandleKeyPressEvent(object sender, KeyPressEventArgs e)
+	{
+		Exit();
+	}
+	
+	public void HandleOnLoadEvent(object sender, EventArgs e)
+	{
+		WindowBorder = WindowBorder.Hidden;
+		WindowState = WindowState.Fullscreen;
+		GL.ClearColor(new Color4(0.1f, 0, 0, 1) );
+	}
+	
+	public void HandleRenderEvent(object sender, FrameEventArgs e)
+	{
+		GL.Clear(ClearBufferMask.DepthBufferBit | 
+		         ClearBufferMask.ColorBufferBit | 
+		         ClearBufferMask.AccumBufferBit | 
+		         ClearBufferMask.StencilBufferBit);
+		SwapBuffers();
+	}
+	
+}
